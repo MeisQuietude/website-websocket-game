aa9b17741257899f8b5241437ac70fea15764040
Stepan Savelyev <stepan.savelyev>
2020-03-18 14:41:01 +0500

    [ADD] Game route, EJS arguments to Index

--- app/src/http/routes.ts
+++ app/src/http/routes.ts
@@ -7,8 +7,17 @@ const router = Router();
 router.use("/static", express.static(path.join(__dirname, "../../resource/public")));
 
 router.get('/', (req: Request, res: Response) => {
-   res.render("index");
+   res.render("index", {});
 });
+
+router.get('/game', (req: Request, res: Response) => {
+   // TODO: Get game by id
+   res.render("game", {fieldLength: 3});
+})
+
+router.post('/game', (req: Request, res: Response) => {
+   // TODO: Post the game
+})
 
 router.all(/.*/, (req: Request, res: Response) => {
    res.status(404).send("Bad request");
