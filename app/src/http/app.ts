d3feac264f0c2c6cc47a2fd4f55a8191f309b730
Stepan Savelyev <mail@s-savelyev.ru>
2020-06-14 23:06:59 +0500

    [UPD] Structure

---
+++ app/src/http/app.ts
@@ -0,0 +1,42 @@
+import * as path from "path";
+
+import express from "express";
+import session from "express-session";
+import morgan from "morgan";
+import nunjucks from "nunjucks";
+
+import routesMiddleware from "./routes/middlewares";
+import routesIndex from "./routes/index";
+import routesGame from "./routes/game";
+import routesBad from "./routes/bad";
+
+const app = express();
+
+app.use(session({
+    secret: "session-secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: app.get("env") === "production",
+    },
+}));
+
+// HTTP Route Logger
+app.use(morgan("dev"));
+
+// Template Engine
+app.set("view engine", "html");
+// app.set("views", path.join(__dirname, "../../resource/views"))
+nunjucks.configure(path.join(__dirname, "../../resource/views"), {
+    autoescape: true,
+    express: app,
+    watch: true,
+    noCache: true,
+});
+
+app.use(routesMiddleware);
+app.use("/", routesIndex);
+app.use("/game", routesGame);
+app.use(routesBad);
+
+export default app;
