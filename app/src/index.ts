0201ba2e0d317104729a4b4b5569108593d66dc7
Stepan Savelyev <mail@s-savelyev.ru>
2020-06-14 19:09:02 +0500

    [IMP] TCP host instead of localhost

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,7 @@
 import serverHTTP from "./http/server";
 import serverIO from "./socket/server";
 
-const express = serverHTTP.listen(3000, () => {
+const express = serverHTTP.listen(3000, "0.0.0.0", () => {
     console.log('Server listening on port 3000!')
     serverIO.attach(express, {
         pingInterval: 10000,
