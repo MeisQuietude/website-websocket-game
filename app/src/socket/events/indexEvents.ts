31a2137973d0904be317dd669953ceb22b8bcc4e
Stepan Savelyev <mail@s-savelyev.ru>
2020-07-05 22:37:24 +0500

    [UPD] Remove useless code

--- app/src/socket/events/indexEvents.ts
+++ app/src/socket/events/indexEvents.ts
@@ -1,4 +1,3 @@
-import Models from "../../data";
 import EventConstructor from "./_eventConstructor";
 
 class Events extends EventConstructor {
@@ -7,12 +6,6 @@ class Events extends EventConstructor {
         this.serverIO.emit("chat", message);
     };
 
-    public createGameInstance = async (): Promise<void> => {
-        const { Game } = Models;
-        const game = new Game({ username: "TestUsername" });
-        await game.save();
-        this.serverIO.emit("createGameDone", game);
-    };
 }
 
 export default Events;
