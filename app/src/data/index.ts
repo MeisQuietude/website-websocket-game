ca60bb08271a629ab7507087fdae38546529c292
Stepan Savelyev <mail@s-savelyev.ru>
2020-07-07 00:26:38 +0500

    [RM] Model Field

--- app/src/data/index.ts
+++ app/src/data/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 
 import { GameModel } from "./models/game";
-import { FieldModel } from "./models/field";
 
 dotenv.config();
 
@@ -9,4 +8,3 @@ const { DBNAME, DBUSER, DBPASS } = process.env;
 export const DBURI = `mongodb+srv://${DBUSER}:${DBPASS}@course-2-vpxyl.azure.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
 
 export const Game = GameModel;
-export const Field = FieldModel;
