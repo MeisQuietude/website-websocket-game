edbce82a377f7fe567da21eb11ba3cb01f688305
Stepan Savelyev <mail@s-savelyev.ru>
2020-07-08 18:01:50 +0500

    [IMP] Default DB is localhost:27017

--- app/src/data/index.ts
+++ app/src/data/index.ts
@@ -5,6 +5,11 @@ import { GameModel } from "./models/game";
 dotenv.config();
 
 const { DBNAME, DBUSER, DBPASS } = process.env;
-export const DBURI = `mongodb+srv://${DBUSER}:${DBPASS}@course-2-vpxyl.azure.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
+
+let dburl = "mongodb://localhost";
+if (DBNAME && DBUSER && DBPASS) {
+    dburl = `mongodb+srv://${DBUSER}:${DBPASS}@course-2-vpxyl.azure.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
+}
+export const DBURI = dburl;
 
 export const Game = GameModel;
